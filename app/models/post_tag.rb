0060c98e9492cbbb0c86fe67a49e9f7bbdc7c5af
EC2 Default User <ec2-user@ip-172-31-12-187.ap-northeast-1.compute.internal>
2024-05-08 14:42:27 +0000

    タグ編集　着手中

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -3,4 +3,23 @@ class PostTag < ApplicationRecord
   belongs_to :tag
   validates :post_id, presence: true
   validates :tag_id, presence: true
+
+  def save
+    post = Post.create(stone_name: stone_name, caption: caption, user_id: user_id, image: image)
+    tag = Tag.where(tag_name: tag_name).first_or_initialize
+    tag.save
+    
+    PostTags.create(post_id: post.id, tag_id: tag.id)
+  end 
+  
+  def update
+    @post = Post.where(id: post_id)
+    post = @post.update(stone_name: stone_name, caption: caption, user_id: user_id, image: image)
+    tag = Tag.where(tag_name: tag_name).first_or_initialize
+    tag.save
+    
+    map = PostTags.where(post_id: post_id)
+    map.update(post_id: post_id, tag_id: tag_id)
+  end 
+  
 end
